9ccd18efe154ae4fd29d00011dc884b94d3a7be2
Your Name <m.gachanga97@gmail.com>
2022-06-14 08:47:05 +0300

    dfdfffdfgff

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Blog extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'placement',
+        'category',
+        'title',
+        'detailOne',
+        'quote',
+        'detailTwo',
+        'image',
+        'imageOne',
+        'imageTwo',
+    ];
 }
