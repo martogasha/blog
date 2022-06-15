f2acc9e8b5e154a74952cf329c4d3d6c2f067e41
Your Name <m.gachanga97@gmail.com>
2022-06-15 13:37:43 +0300

    first commi

--- app/Http/Controllers/DetailController.php
+++ app/Http/Controllers/DetailController.php
@@ -9,7 +9,7 @@ class DetailController extends Controller
 {
     public function detail($id){
         $detail = Blog::find($id);
-        $eights = Blog::where('placement','eight')->where('id','!=',$id)->latest('id')->paginate(8);
+        $eights = Blog::where('placement',$detail->placement)->where('id','!=',$id)->latest('id')->paginate(8);
         $sixs = Blog::where('placement','eight')->where('id','!=',$id)->latest('id')->paginate(2);
         return view('detail',[
             'detail'=>$detail,
