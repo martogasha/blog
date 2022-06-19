c5ace9252a8712e3f5dae4ba31b88ccf54ab9deb
Your Name <m.gachanga97@gmail.com>
2022-06-19 10:10:22 +0300

    firstdfjkhdfjksnbdfs

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
+    <link rel="icon" type="image/x-icon" href="{{asset('images/fav.png')}}">
 
     <link href="https://fonts.googleapis.com/css?family=Encode+Sans+Expanded:400,600,700" rel="stylesheet">
 
