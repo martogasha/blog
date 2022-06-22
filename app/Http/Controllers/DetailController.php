161a3353ff1413958151d789f200815c36aa72af
Your Name <m.gachanga97@gmail.com>
2022-06-22 11:30:45 +0300

    sdjkfhdjdjdfkl

--- app/Http/Controllers/DetailController.php
+++ app/Http/Controllers/DetailController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 class DetailController extends Controller
 {
     public function detail($id,$name){
+        dd($id);
         $detail = Blog::find($id);
         $on = Blog::where('placement','one')->latest('id')->first();
         $son = Blog::where('placement','seven')->latest('id')->first();
